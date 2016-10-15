517bf8c54f7ebf451fa214c1c4f76b2001b9de69
wu_wei <povesummer@163.com>
2016-10-15 23:44:37 +0800

    增加关于字段

--- website/models/video.py
+++ website/models/video.py
@@ -7,7 +7,9 @@ class Video(models.Model):
     content = models.TextField(null=True, blank=True)
     url_image = models.URLField(null=True, blank=True)
     editor_choice = models.BooleanField(default=False)
-    
+    user = models.ManyToManyField(to=UserProfile, null=True, blank=True)
+    is_collect = models.BooleanField(default=False)
+
     def __str__(self):
         return self.title
 
